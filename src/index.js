c7aaa5680eee8369171696cf02eafd3add0dc453
samroodAli <devsamrood@gmail.com>
2021-09-08 15:52:50 +0530

    StaticComponents:Create App component and import in index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom'
+import App from "./components/App"
 
 const root = document.getElementById('root')
-ReactDOM.render(<h1>Hello world</h1>,root)
+ReactDOM.render(<App/>,root)
