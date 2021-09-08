8ed221092fa571b380c902cf7f8c4ba71a0e83f2
samroodAli <devsamrood@gmail.com>
2021-09-08 15:56:45 +0530

    StaticComponents:Create SearchBar with controlled state and onSubmit prop

---
+++ src/components/SearchBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = {
+    term: ""
+  };
+
+  setTerm = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Youtube Search</label>
+            <input
+              text=""
+              type="text"
+              placeholder="Search here"
+              value={this.state.term}
+              onChange={this.setTerm}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
