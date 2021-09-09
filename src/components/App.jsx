01cb618b16322d40496dd17ff9b90aa67c674535
samroodAli <devsamrood@gmail.com>
2021-09-09 14:04:13 +0530

    State:Create state for holding videos with default array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
 
 class App extends React.Component {
-  onSearchSubmit = (term) => {
-    console.log(term);
+  state = { videos: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
   };
 
   render() {
